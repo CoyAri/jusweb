ac2335aaeaf1c2e15df74af6e964d4930ecdd70c
CoyAri <91406190+CoyAri@users.noreply.github.com>
2022-04-17 15:36:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function contactPop()
+{
+    console.log(1);
+    var input = document.getElementById('hour-rate');
+    if(document.getElementById('hiring').checked)
+    {
+        input.style.display = 'inline';
+    }
+    else input.style.display = 'none';
+}
